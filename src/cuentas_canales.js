4a31f15a13d2cac9fdf10b14b9f19bb563a6043a
keplergit <kepler6.023@gmail.com>
2022-10-01 16:34:23 -0400

    primera subida proyecto busca yt_5

---
+++ src/cuentas_canales.js
@@ -0,0 +1,141 @@
+// cuentas de la mil putas y los canales donde esa perra posiblemente ande
+module.exports = {
+    cuentas: [
+        "UCeE8-d3AVtpaQTqKsnlTbng",
+        "UCCyezB3yAjtexgicVam6bVg",
+        "UCD8xxEDZXQqTG1DNeT5atlg",
+        "UChZFFCN6C4Q-_JaFHWOexIA",
+        "UCeTWwSd6GVtFzhBHffi0vYA", // MIL PUTAS karla pinzon
+        "UCuBsWgcxKURobCINxa1UP2w", // MIL PUTAS ANOLA
+        "UCX4he3CGIr4IoCH74hs_QHQ", // MIL PUTAS ANOLA 2
+        "UCiMuc3hL8SX1lnjeGa6xdUA", //mil putas mora
+        "UC4FDfG0svLWr8UTygKkQROA", // indio mauri
+        "UCJjl62B_-CD5GjEtrHBxKUA", // maya
+        "UC3zJpFt60eeRDkdOqy3KEkQ", // MANUELA72
+        "UCK6wIQRDuAzeUs_8vS07OIQ", // EMILSE
+        "UCbylwOYa1kFfNeos7JOf5nQ", // INDIO GUSANO
+        "UCBIre3tdX3Se-fmnzI23nDg", // jen 1
+        "UCKajy1eaKJUw1vzpX_9452A", // lorna
+        "UCMcwTQrLBq2NuhB9zQeD19w", // india manuela
+        "UCcNrEm1UFcsVEfwOU7EY40Q", // jen 2
+        "UCBmtccczh1hwI0i5vJN_LlA", // LIGE
+        "UCa8f35K9tg4fOd4GT2WP3Xg", // krishna
+        "UCd7dk1mATeIKbYSkxMf45kw", // JOSE VAZQUES
+        "UCjrm1ZD6Il5cr0G4oYc7UMQ", // ROMI FLOR
+        "UCZeVURjSotPIBceRC7V-s8g", // DE LA MIL PUTAS TONI KASS
+        "UCHAaTBpnDSvjuvpqxr_RStg", // INDIO DE MIL PUTAS CRIS-7
+        "UCicMVPurEMSqa6x5Hl_XdvA", // INDIO DE MIL PUTAS MOROCHO
+        "UCjq1HzoMiBL8GlHYK8pMwzQ", // QUEEN
+        "UCRe4Q2ysLU7GXzKaT3ggSjA", // DANNII INDIO
+        "UCvF2JQvx0OIOZNK-VXe01HA", // M INDIA
+        "UCWGJfJw6JYzDJzTA6W54QWg", // I INDIA
+        "UCQ5qAXoJARiZRKtsmzRLokA", // INDIA CLEO
+        "UC9UPQ6RHtFZlq-ebU2l7_0A", // INDIO MAXIMO
+        "UCzdo-OD06LR-r8VmqUg3BFA", // RONALD POCOACA
+        "UCXwa98DkprbI2K1NMbbUJ5w", // INDIA KAREM XXX
+        "UC_CRHkQY0LXdcAeabMv4Z4g", // Natalia Aquino
+        "UCaPg7130hLsUrWTeSHfAfzA", // PUTILLA LUZ 13
+        "UCUMDeSO4H0-LsL__JR9qujQ", // LILI 1
+        "UCLq3dSyKhlpQ8TXn0MZKbgg", // LILI 2
+        "UC6W4mI1zzCJ2iaZfP04mSMw", // INDIO ANGEL SERA DE LA MIL PUTAS?
+        "UCEg-cFulH2PhBJtacGNwbRg", // PUTA ROSE 100
+        "UCRSvSWtEVPKdMERtmmQVqsQ", // INDIO MANOLO DE BOSSA
+        "UC3SeHEiHWaKF3QoIQRz6x9A", // INDIO CREADA POR MIL PUTAS 1
+        "UCEsnpcl4gaXVPnl3zvbOaZg", // INDIO CREADA POR MIL PUTAS 2
+        "UCCIxz3HUoPCRcahgI-LTFjw", // FLAVIA SUAREZ
+        "UCf2Xfq8Dqy2JBcDC_vhoH9Q", // SOFIA 80S
+        "UC991nWBJ1q3ExleBtzElQgg", // ROS GUTIERRES BOLIVIANA AMIGA DE MIL PUTAS
+        "UCEqJln8Yx0rMOv2di0tPB6Q", // uwu isa
+        "UC9eMhjHSbAfzcrJIQcgD35g", // indio hans de isa
+        "UCDtlMvO_QnQCDhTf4dJMLjg", // isa puta vampira
+        "UCLRf62U3V5dsNVuR0cMz9OA", // jesus de isa puta
+        "UCOfuKXEa7MNwisbWg66LuKg", // noha de isa puta
+        "UCQ6lszpfl2wwfey4bdw7IxQ", // memori codeo de isa puta
+        "UCVRKc9Pcie_BnsPB3oZwcKw", // pepe churro indio de puta isa
+        "UCjGw0YxhH8IzCHGqs2ym6Nw", // kitti cat de puta isa
+        "UCFXQaOKSsSuTfFGfGRC9n4A", // pumuki chido de puta isa
+        "UC9E0Qu9QhOQKPGURGJnNulw", // Jim3 (la 100)
+        "UClPSX6zsTm_AX634xYFrKEA", // CArmen ochoa
+        "UCD8A1GtRgkMTgXEHnmhISqg", // sofi sweet
+        "UCtN0UARP60z5WNePkYcgHGA", // yoana g
+        "UCHGLvsUrym0gTTEnerO-6Kw", // karem mio
+        "UC8FnmZjIX4M_fUP86gXhRxw", // ROSSI india mauri
+        "UCkR6cQGPhxwfSqCq5K9SBeQ", // MIO CLIN BARRIL
+    ],
+
+    canales: [
+/*
+        "UCvNogbKXdmFScYgEgLNU_4g", // larissa (80s)
+        "UCpxY5zDygRzDIP5bTtTI5rA", // italo disco (80s)
+        "UCxBAtJ0GmD6mhPlidFttkNA", // disco hits (80s)
+        "UCLo_UlgP6CF8sgAjkDM3gJw", // disco music (80s)
+        "UC-ITe1nKavRL6-Sl2aE3LKA", // music hits (80s)
+        "UCnrM0k6ORcRDod2ytUZ4b7g", // Golden Disco Dance (80s)
+        "UCeba3pJpMflswMBIANH1u7w", // Best Slow Rock (MAYA) (80s)
+
+        "UC3PTdlO_kKsClMt8hQ3w0PQ", // musica intemporal (LATINA)
+        "UCZWyDlLgsZxwYn_HeGDpptg", // BACHATA MELODIA, (LATINA)
+        "UCKBnLtJy1Z82hyGWBJkWbsw", // MUSICA BACHATA (LATINA)
+        "UC-yOf9S1O_UG67nTe7ifrJQ", // BACHATA MIX (LATINA)
+        
+        
+        
+        "UCHEiLo-jHpipaO7lYX4DnTQ", // MELODIA MUSICAL (LATINA)
+        "UC2Y1w-M6jxiezn-_IEPfwFA", //Muzik Express (LATINAS)
+        "UCIDO7u5X7sXXyqVU_KA4GwQ", // Romanticas Música (LATINA)
+        "c/BaladasRomanticasÉxitos", // Baladas Romanticas Éxitos (LATINA)
+
+        "c/JAZZBLUES37999", // JAZZ Y BLUES (JAZZ)
+        "UC7bX_RrH3zbdp5V4j5umGgw", // relax music (jazz paris) (JAZZ)
+        "UCeGJ6v6KQt0s88hGKMfybuw", // Cozy Jazz Music (JAZZ)
+
+        "c/PMBMusic", // PMB MUSIC (cristo rio bossa nova) (RELAJANTE)
+        "c/musicbrokers", // music brokers (RELAJANTE)
+        "c/PlaylistsKool", // bossa arena cafe playa (RELAJANTE)
+        "c/ILoveYouVenice", // venecia vivo (RELAJANTE)
+        "c/Teleportcamera", // Teleport.camera (RELAJANTE)
+        
+
+
+        
+        "c/JackFrancisComposer", // OCB Relax Music (RELAJANTE)
+        "user/YourRelaxMusic1", // Música Relajante, de Meditación, Música para Dormir (RELAJANTE)
+        "c/CassioToledo", // CASSIO TOLEDO (CALMAR) (RELAJANTE)
+        "c/PianoMusica", // PIANO MUSICA (RELAJANTE)
+        "UC_GGWVDrDwGAunoFRiWbHDw", // soul relajante (RELAJANTE)
+        "UCS4kVH1srJ9h9nqJPG2iDLg", // sat-chit musica y mantras (RELAJANTE)
+        "c/MellowDream", // relajante (ford comander) (RELAJANTE)
+        "UCy-jpxAHSt4wz_eruVuz_4A", // RELAX TIME (relajante ruso) (RELAJANTE)
+        "UCMNiaUs8-Lb5TzwhjLcswAw", // 4K Scenic Relaxation (relajante ruso) (RELAJANTE)
+        "UCsHOUGUpmJo73KONPz3DTkQ", // Lucid Dreaming Music (relajante) (RELAJANTE)
+        "UCmZzJBHL3kC1z2PwsJOxFxA", // QT Relax Music (RELAJANTE)
+        
+        
+        "UCC0XEDBRtOO2GPC_DP4hR3w", // Beautiful Melody (RELAJANTE)
+        "c/SoothingRelaxation", // Soothing Relaxation (RELAJANTE)
+        "c/MusicaRelajanteBeautifulDreams", // Musica Relajante - Beautiful Dreams (RELAJANTE)
+        "UC_G6q3LhFZznIrVHL6oK8bA", // Healing Melody (RELAJANTE)
+        "UC4L-dSrzbPoZcr1Av5GvwKw", // Open Heart Music - Helios 4K (RELAJANTE)
+        "UC9w8My3S7h-bQZ-4R-0ZPsw", // Helios Music (RELAJANTE)
+        "c/timjanis", // Tim Janis (RELAJANTE)
+        "UCfAqGlaQDI4uNrezkuuVYIQ", // Soothing Meditation Music (RELAJANTE)
+        "c/AlexdelosReyesmusicTHR", // Healing Your Spirit (RELAJANTE)
+        "user/muljanic", // Relax Your Mind (RELAJANTE)
+        "UCRZnWilWTPU50kZxlGaC_nQ", // Coffee Music TBM (RELAJANTE)
+        */
+        
+        "c/OddEagleProductions", // Odd Eagle (RELAJANTE)
+        "UCv4KQtNQ3bS5jPCOa6F9LBw", // Music Heals the Soul (RELAJANTE)
+        "UC_18SY0IqUBsVX3vyjoT8BQ", // Healing Your Spirit (RELAJANTE)
+        "UCTiXaqByXkH-h97DLlLkx1Q", //Queen Club (RELAJANTE)
+        "UCJW3ePyqjsvKfHGsz7eetQA", // wonder deep (RELAJANTE)
+        "c/TheGoodLiferadio", // The Good Life Radio x Sensual Musique (RELAJANTE)
+        "UCWal9QzIup8uxqWbI30JVyA", // Music Hits Ever (RELAJANTE)
+        "UC6g_Yf3YTagd95_DYFX6Yrw", // Music Hits All Time BOD MARLEY (RELAJANTE)
+        "c/MagicSpider", // Wonder EDM (RELAJANTE)
+
+        "c/Gentleman1", // gentleman (HOUSE)
+        "user/dav0delik", //XDeep Club (HOUSE)
+        "c/MusicLabAmbient", // 3 DIRECTOS SOLO PARA PRUEBAS
+    ],
+};
